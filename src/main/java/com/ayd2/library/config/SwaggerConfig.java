6bd9a1e6815c620e46a46d98bb1a245c0de8f825
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-03-23 17:37:02 -0600

    add: swagger ui

---
+++ src/main/java/com/ayd2/library/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.ayd2.library.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("CUNOC Library API")
+                        .version("1.0")
+                        .description("REST API designed for the CUNOC Library system.This API provides endpoints to manage library resources such as books, users, loans, and returns."));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
