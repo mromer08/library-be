add711a5b35fdc958c1f913cbea7133ce71105bf
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-03-24 00:39:55 -0600

    update: degrees validation

--- src/main/java/com/ayd2/library/dto/degrees/NewDegreeRequestDTO.java
+++ src/main/java/com/ayd2/library/dto/degrees/NewDegreeRequestDTO.java
@@ -1,14 +1,17 @@
 package com.ayd2.library.dto.degrees;
 
-import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public record NewDegreeRequestDTO(
     @NotNull(message = "Code is required")
-    @Digits(integer = 5, fraction = 0, message = "Code must be a number with a maximum of 5 digits")
+    @Positive(message = "Code must be a positive number")
+    @Min(value = 10, message = "Code must be a number with a minimum of 2 digits")
     Long code,
+    
     @NotBlank(message = "Name is required")
     @Size(min = 5, max = 255, message = "Name must be between 5 and 255 characters")
     String name
--- src/main/java/com/ayd2/library/dto/degrees/UpdateDegreeRequestDTO.java
+++ src/main/java/com/ayd2/library/dto/degrees/UpdateDegreeRequestDTO.java
@@ -1,10 +1,12 @@
 package com.ayd2.library.dto.degrees;
 
-import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public record UpdateDegreeRequestDTO(
-    @Digits(integer = 5, fraction = 0, message = "Code must be a number with a maximum of 5 digits")
+    @Positive(message = "Code must be a positive number")
+    @Min(value = 10, message = "Code must be a number with a minimum of 2 digits")
     Long code,
 
     @Size(min = 5, max = 255, message = "Name must be between 5 and 255 characters")
