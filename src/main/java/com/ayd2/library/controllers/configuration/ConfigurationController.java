f55509c6cffe603f56a599069e920dd77ac233a3
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-03-23 19:01:50 -0600

    add: configuration controller

---
+++ src/main/java/com/ayd2/library/controllers/configuration/ConfigurationController.java
@@ -0,0 +1,33 @@
+package com.ayd2.library.controllers.configuration;
+
+import com.ayd2.library.dto.configurations.ConfigurationResponseDTO;
+import com.ayd2.library.dto.configurations.UpdateConfigurationRequestDTO;
+import com.ayd2.library.exceptions.NotFoundException;
+import com.ayd2.library.services.configuration.ConfigurationService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/configuration")
+@RequiredArgsConstructor
+public class ConfigurationController {
+
+    private final ConfigurationService configurationService;
+
+    @GetMapping
+    public ResponseEntity<ConfigurationResponseDTO> getConfiguration() throws NotFoundException {
+        ConfigurationResponseDTO response = configurationService.getConfiguration();
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping
+    public ResponseEntity<ConfigurationResponseDTO> updateConfiguration(
+            @RequestBody @Valid UpdateConfigurationRequestDTO updateRequest) throws NotFoundException {
+        ConfigurationResponseDTO response = configurationService.updateConfiguration(updateRequest);
+        return ResponseEntity.ok(response);
+    }
+}
