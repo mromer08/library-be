06b50c66ce0b4deb958d315a48d555cc625ed5a8
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-03-29 10:03:24 -0600

    update: student debt column deleted

--- src/main/java/com/ayd2/library/dto/students/StudentDetailResponseDTO.java
+++ src/main/java/com/ayd2/library/dto/students/StudentDetailResponseDTO.java
@@ -1,6 +1,5 @@
 package com.ayd2.library.dto.students;
 
-import java.math.BigDecimal;
 import java.util.UUID;
 
 import com.ayd2.library.dto.users.UserDetailResponseDTO;
@@ -9,7 +8,6 @@ import com.ayd2.library.models.degree.Degree;
 public record StudentDetailResponseDTO(
     UUID id,
     Boolean isSanctioned,
-    BigDecimal debt,
     Long carnet,
     UserDetailResponseDTO user,
     Degree degree
