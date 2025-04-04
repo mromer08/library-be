6d2c018e0782157c02156347ebbf7467c01779fb
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-04-04 07:41:38 -0600

    update: repoort service impl

--- src/main/java/com/ayd2/library/services/report/ReportServiceImpl.java
+++ src/main/java/com/ayd2/library/services/report/ReportServiceImpl.java
@@ -2,6 +2,7 @@ package com.ayd2.library.services.report;
 
 import java.time.LocalDate;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,72 +14,76 @@ import com.ayd2.library.dto.report.RevenueReportDTO;
 import com.ayd2.library.dto.report.TopDegreeInLoansReportDTO;
 import com.ayd2.library.dto.report.TopStudentLoansReportDTO;
 import com.ayd2.library.dto.students.StudentResponseDTO;
-import com.ayd2.library.models.student.Student;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(rollbackFor = Exception.class)
-public class ReportServiceImpl implements ReportService {@Override
-    public PagedResponseDTO<LoanResponseDTO> findLoansByDueDate(LocalDate date) {
+@Transactional(rollbackFor = Exception.class, readOnly = true)
+public class ReportServiceImpl implements ReportService {
+    
+    @Override
+    public PagedResponseDTO<LoanResponseDTO> findLoansByDueDate(LocalDate date, Pageable pageable) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'findLoansByDueDate'");
     }
 
     @Override
-    public PagedResponseDTO<LoanResponseDTO> findOverdueLoans() {
+    public PagedResponseDTO<LoanResponseDTO> findOverdueLoans(Pageable pageable) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'findOverdueLoans'");
     }
 
     @Override
-    public RevenueReportDTO generateRevenueReport(LocalDate startDate, LocalDate endDate) {
+    public RevenueReportDTO generateRevenueReport(LocalDate startDate, LocalDate endDate, Pageable pageable) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'generateRevenueReport'");
     }
 
     @Override
-    public TopDegreeInLoansReportDTO findTopDegreeInLoans(LocalDate startDate, LocalDate endDate) {
+    public TopDegreeInLoansReportDTO findTopDegreeInLoans(LocalDate startDate, LocalDate endDate, Pageable pageable) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'findTopDegreeInLoans'");
     }
 
     @Override
-    public OverduePayStudentReportDTO findOverduePaymentsByStudent(Student student, LocalDate startDate,
-            LocalDate endDate) {
+    public OverduePayStudentReportDTO findOverduePaymentsByStudent(Long studentCarnet, LocalDate startDate,
+            LocalDate endDate, Pageable pageable) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'findOverduePaymentsByStudent'");
     }
 
     @Override
-    public TopStudentLoansReportDTO findTopStudentInLoans(LocalDate startDate, LocalDate endDate) {
+    public TopStudentLoansReportDTO findTopStudentInLoans(LocalDate startDate, LocalDate endDate, Pageable pageable) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'findTopStudentInLoans'");
     }
 
     @Override
-    public PagedResponseDTO<BookResponseDTO> findBooksCurrentlyBorrowedByStudent(Student student) {
+    public PagedResponseDTO<BookResponseDTO> findBooksCurrentlyBorrowedByStudent(Long studentCarnet,
+            Pageable pageable) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'findBooksCurrentlyBorrowedByStudent'");
     }
 
     @Override
-    public PagedResponseDTO<BookResponseDTO> findOutOfStockBooks() {
+    public PagedResponseDTO<BookResponseDTO> findOutOfStockBooks(Pageable pageable) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'findOutOfStockBooks'");
     }
 
     @Override
-    public PagedResponseDTO<BookResponseDTO> findNeverBorrowedBooks() {
+    public PagedResponseDTO<BookResponseDTO> findNeverBorrowedBooks(Pageable pageable) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'findNeverBorrowedBooks'");
     }
 
     @Override
-    public PagedResponseDTO<StudentResponseDTO> findSanctionedStudents() {
+    public PagedResponseDTO<StudentResponseDTO> findSanctionedStudents(Pageable pageable) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'findSanctionedStudents'");
     }
+    
+
     
 }
